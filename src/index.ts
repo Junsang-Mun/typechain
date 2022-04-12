ee39671dbbd35e2e19611e8208c5603b0e017066
Junsang-mun <mun.js@aol.com>
2022-04-12 20:52:55 +0900

    class, public, constructor, this...

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,26 @@
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+}
+
+const lynn = new Human("Lynn", 1, "eagle");
+
 const person = {
   name: "Lee Sang Hae Ssi",
   gender: "Pokémon",
   age: 1,
 };
 
-const sayHi = (name: string, age: number, gender: any): string => {
-  return `Hi, you are ${name} and you are ${age} years old. And you are ${gender}!`;
+const sayHi = (person: Human): string => {
+  return `Hi, you are ${person.name} and you are ${person.age} years old. And you are a ${person.gender}!`;
 };
 
-console.log(sayHi("name", 123, "asdf"));
+console.log(sayHi(lynn));
 
 export {};
