37440d43f0a020b6395de756843311757b1bf395
Junsang-mun <mun.js@aol.com>
2022-04-12 18:49:52 +0900

    function return value

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const name = "Kim";
 const age = -99;
 const gender = null;
 const sayHi = (name, age, gender) => {
-    console.log(`Hi, you are ${name} and you are ${age} years old. And you are ${gender}`);
+    return `Hi, you are ${name} and you are ${age} years old. And you are ${gender}`;
 };
-sayHi(name, age, gender);
+console.log(sayHi(name, age, gender));
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -2,12 +2,10 @@ const name = "Kim";
 const age = -99;
 const gender = null;
 
-const sayHi = (name: string, age: number, gender: any) => {
-  console.log(
-    `Hi, you are ${name} and you are ${age} years old. And you are ${gender}`
-  );
+const sayHi = (name: string, age: number, gender: any): string => {
+  return `Hi, you are ${name} and you are ${age} years old. And you are ${gender}`;
 };
 
-sayHi(name, age, gender);
+console.log(sayHi(name, age, gender));
 
 export {};
