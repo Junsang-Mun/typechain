a7277faded2791c27072f8265d374eecda60a7c5
Junsang-mun <mun.js@aol.com>
2022-04-12 18:48:08 +0900

    sayHi function

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 "use strict";
-console.log("Hello");
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Kim";
+const age = -99;
+const gender = null;
+const sayHi = (name, age, gender) => {
+    console.log(`Hi, you are ${name} and you are ${age} years old. And you are ${gender}`);
+};
+sayHi(name, age, gender);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("Hello");
+const name = "Kim";
+const age = -99;
+const gender = null;
+
+const sayHi = (name: string, age: number, gender: any) => {
+  console.log(
+    `Hi, you are ${name} and you are ${age} years old. And you are ${gender}`
+  );
+};
+
+sayHi(name, age, gender);
+
+export {};
